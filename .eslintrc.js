bee416d9b205fce72f0ccdfe479fe5c15aa92cc5
ssi02014 <ssi02014@naver.com>
2023-03-13 02:40:35 +0900

    feat: eslint-plugin-prettier 추가

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,17 +5,23 @@ module.exports = {
     node: true,
   },
   parser: '@typescript-eslint/parser',
-  plugins: ['@typescript-eslint', 'react', 'react-hooks'],
+  plugins: [
+    '@typescript-eslint',
+    'react',
+    'react-hooks',
+    'prettier', // eslint-plugin-prettier: ESLint 규칙에 Prettier의 규칙을 적용
+  ],
   extends: [
     'eslint:recommended',
     'plugin:react/recommended',
     'plugin:@typescript-eslint/recommended',
-    'prettier',
+    'plugin:prettier/recommended', // eslint-config-prettier: Prettier의 설정과 충돌하는 ESLint의 규칙을 자동으로 비활성화
   ],
   rules: {
-    '@typescript-eslint/no-var-requires': 'off', // require 쓰는 것을 방지하는 옵션인데 끔
-    '@typescript-eslint/explicit-function-return-type': 'off', // 함수 반환 타입 지정 끔
+    '@typescript-eslint/no-var-requires': 'off', // require 쓰는 것을 방지
+    '@typescript-eslint/explicit-function-return-type': 'off', // 함수 반환 타입 지정
     '@typescript-eslint/explicit-module-boundary-types': 'off',
-    '@typescript-eslint/no-explicit-any': 'off', // any 타입사용을 방지하는 옵션 끔
+    '@typescript-eslint/no-explicit-any': 'off', // any 타입사용을 방지
+    'prettier/prettier': 'error', // Prettier와 충돌하는 규칙을 error
   },
 };
