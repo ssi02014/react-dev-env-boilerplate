ff35c5ae1ae79c84ff50438bd54f8296ea384468
ssi02014 <ssi02014@naver.com>
2023-03-27 19:01:55 +0900

    fix: file-loader url-loader 제거

--- babel.config.js
+++ babel.config.js
@@ -1,14 +1,10 @@
 // jest를 위한 babel config
+// jest는 babel-jest를 통해 기본적으로 babel.config를 읽어들임
 module.exports = {
   presets: [
     '@babel/preset-env',
     '@babel/preset-typescript',
-    [
-      '@babel/preset-react',
-      {
-        runtime: 'automatic', // jest로 테스트 시 React 컴폰너트 읽기 위함.
-      },
-    ],
+    ['@babel/preset-react', { runtime: 'automatic' }], // jest로 테스트 시 React 컴폰너트 읽기 위함.
   ],
   plugins: [],
 };
