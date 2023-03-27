da9468a89e92e19e5aa2e9e648ceea18dafe53e1
ssi02014 <ssi02014@naver.com>
2023-03-27 18:51:34 +0900

    fix: file-loader, url-loader 제거 및 babel config corejs@3 적용

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   presets: [
-    '@babel/preset-env',
+    ['@babel/preset-env', { useBuiltIns: 'usage', corejs: 3 }],
     '@babel/preset-typescript',
     [
       '@babel/preset-react',
