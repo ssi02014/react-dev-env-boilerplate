3a8be4947bc61fa170f866e7ab4f6065cd91ef2e
ssi02014 <ssi02014@naver.com>
2023-04-19 20:47:03 +0900

    feat: video asset 추가 및 minify 중복 옵션 제거

--- src/types/index.d.ts
+++ src/types/index.d.ts
@@ -32,3 +32,13 @@ declare module '*.svg?url' {
   const content: string;
   export default content;
 }
+
+declare module '*.mp4' {
+  const content: string;
+  export default content;
+}
+
+declare module '*.webm' {
+  const content: string;
+  export default content;
+}
